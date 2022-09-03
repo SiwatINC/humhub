95740e2d2800caca42d46fae3d2cc9c1054a9590
Siwat Sirichai <siwat@siwatinc.com>
2022-09-04 00:02:05 +0700

    Update initialize-builtin.sh

--- initialize-builtin.sh
+++ initialize-builtin.sh
@@ -19,16 +19,17 @@ if [ -z "$(ls -A /config)" ]; then
    cp /initializer/default /config/nginx/site-confs/default
    cp -rv /web-source/* /config/www/
    chmod +x /config/www/protected/yii
-   mysqld &
+   service mysql start
    cat /initializer/initializedb.sql | mysql
    cron &
    crontab /var/spool/cron/crontabs/root
+   chmod 777 -R /config/www
    echo "; Add any php.ini entry here to overwrite any php.ini configuration" > /config/php/php-overwrite.ini
    echo "Please replace yourdomain.com with your domain in /config/nginx/site-confs/default to enable pagespeed."
 else
    echo "Already Initiated, Starting NGINX PageSpeed and PHP7.4"
    mkdir -p /var/cache/nginx | :
-   mysqld &
+   service mysql start
    echo "If you haven't yet, please replace yourdomain.com with your domain in /config/nginx/site-confs/default to enable pagespeed."
    cat /config/php/php-overwrite.ini >> /etc/php/7.3/fpm/php.ini
    cron &
